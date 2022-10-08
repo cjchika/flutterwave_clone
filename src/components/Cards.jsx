81217b18373eec60c5e1d187e368b2bfc8f9f157
Chijioke Chika <8pluscreative@gmail.com>
2022-10-08 18:10:14 +0100

    Cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -27,22 +27,11 @@ const Cards = () => {
               src={MasterCard}
             />
           </div>
-          <div className="flex flex-col md:flex-row w-full justify-center relative ">
-            <div className="text-white bg-[url('../assets/Images/box.png')] z-[1]  bg-[#313131] rounded-[1.6rem] shadow-md overflow-hidden text-left p-14">
-              <h6 className="text-[1.2rem] mb-[1.5rem]">
+          <div className="flex flex-col md:flex-row w-full justify-center  ">
+            <div className="text-white bg-[url('./assets/Images/box.png')] z-[1]  bg-[#313131] rounded-[1.6rem] drop-shadow-2xl overflow-hidden text-left p-8 lg:p-12 md:absolute md:w-[30rem] md:ml-[-15rem] lg:ml-[-20rem]">
+              <h6 className="font-semibold text-[1.2rem] mb-[1.5rem]">
                 Better Business Expense management
               </h6>
-              <p className="text-[1rem] leading-[1.5rem]">
-                You can issue cards for specific business purposes so you can
-                better manage your expenses. An example of a business expense
-                you’re able to better manage now is your Instagram ad. You’re
-                able to issue a specific virtual card for all your Instagram ads
-                to better manage and account for that expense within your
-                business.
-              </p>
-            </div>
-            <div className="text-white z-[2] bg-[url('../assets/Images/box.png')] my-[3rem] md:my-0 bg-[#313131] rounded-[1.6rem] shadow-md overflow-hidden text-left p-14">
-              <h6 className="text-[1.2rem] mb-[1.5rem]">New revenue lines</h6>
               <p className="text-[1rem] leading-[1.5rem]">
                 You can issue a physical bank card to an employee or even a
                 trusted merchant at your local market to make the purchases for
@@ -51,8 +40,20 @@ const Cards = () => {
                 customer. The possibilities are endless.
               </p>
             </div>
-            <div className=" text-white z-[2] bg-[url('../assets/Images/box.png')] bg-[#313131] rounded-[1.6rem] shadow-md overflow-hidden text-left p-14">
-              <h6 className="text-[1.2rem] mb-[1.5rem]">
+            <div className="text-white z-[2] bg-[url('./assets/Images/box.png')] my-[3rem] md:my-0 bg-[#313131] rounded-[1.6rem] drop-shadow-2xl overflow-hidden text-left p-8 lg:p-12 md:absolute  md:w-[30rem] md:ml-[2rem] lg:ml-4rem]">
+              <h6 className="font-semibold text-[1.2rem] mb-[1.5rem]">
+                New revenue lines
+              </h6>
+              <p className="text-[1rem] leading-[1.5rem]">
+                You can issue a physical bank card to an employee or even a
+                trusted merchant at your local market to make the purchases for
+                you. Every time you have an order, you’re able to fund that card
+                so they can purchase it and send it to you for delivery to your
+                customer. The possibilities are endless.
+              </p>
+            </div>
+            <div className=" text-white z-[3] bg-[url('./assets/Images/box.png')] bg-[#313131] rounded-[1.6rem] drop-shadow-2xl overflow-hidden text-left p-8 lg:p-12 md:w-[30rem] md:ml-[15rem] lg:ml-[14rem]">
+              <h6 className="font-semibold text-[1.2rem] mb-[1.5rem]">
                 Seamless Business Organization
               </h6>
               <p className="text-[1rem] leading-[1.5rem] ">
